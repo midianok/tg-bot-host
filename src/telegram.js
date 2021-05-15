2c11556c3252f1c83dd5393183731f8022280c1b
Ilya Gusev <igusev2@alfabank.ru>
2021-05-15 23:43:15 +0300

    Mongo configs

--- src/telegram.js
+++ src/telegram.js
@@ -1,12 +1,37 @@
 const { Telegraf } = require('telegraf');
+const MongoClient = require('mongodb').MongoClient;
 
 const runTgBot = async (config) => {
-    const bot = new Telegraf(config.token);
-    bot.start((ctx) => ctx.reply('Starting echo'));
+    const client = new MongoClient(config.connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    bot.on('text', ctx => ctx.telegram.sendMessage(ctx.chat.id, ctx.message.text) );
+    client.connect(async err => {
+        const collection = client.db(config.dbName).collection(config.collectionName);
+        const bots = await collection.find({}).toArray();
+        for (const botConfig of bots) {
+            const bot = new Telegraf(botConfig.token);
 
-    await bot.launch();
+            for (const action of botConfig.actions) {
+                if (action.on === 'text'){
+                    bot.on(action.on, ctx => ctx.reply(action.reply));
+                }
+
+                if (action.on === 'new_chat_members'){
+                    bot.on(action.on, (ctx) => {
+                        ctx.message.new_chat_members.forEach(function (user) {
+                            const message = action.reply.replace("{user}", `@${user.username}`)
+                            ctx.reply(message);
+                        });
+                    })
+                }
+            }
+
+            bot.launch();
+
+            process.once('SIGINT', () => bot.stop('SIGINT'))
+            process.once('SIGTERM', () => bot.stop('SIGTERM'))
+        }
+        await client.close();
+    });
 }
 
 module.exports.runApp = runTgBot;
