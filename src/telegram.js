184ac4b9a8ea1120a441d8f883ef8b513dbf3507
Ilya Gusev <igusev2@alfabank.ru>
2021-05-31 23:50:05 +0300

    Concat transcriptions

--- src/telegram.js
+++ src/telegram.js
@@ -45,7 +45,8 @@ const runTgBot = async (config) => {
                             };
 
                             const [response] = await client.recognize(request);
-                            const transcription = response.results[0]?.alternatives[0]?.transcript;
+                            const transcription = response.results.map(x => x.alternatives.map(z => z.transcript)).join(" ");
+
                             const result = transcription ?
                                 `Вот что наговорил\\_a *${ctx.update.message.from.first_name}* \\(а лучше бы жопу скинул\\_a\\):\n_${transcription.replaceAll("-", "\\-")}_` :
                                 `Ничего не понятно что вы там *${ctx.update.message.from.first_name}* ноговорил\\_a, если честно, лучше бы жопу скинул\\_a`;
