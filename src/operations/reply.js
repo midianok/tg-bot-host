a22d446ededc448e5a6e4752429b40d635ace384
Ilya Gusev <igusev2@alfabank.ru>
2021-06-29 19:54:11 +0300

    reply regexp impl

--- src/operations/reply.js
+++ src/operations/reply.js
@@ -2,5 +2,13 @@ const { getRandomElement } = require("./../util/getRandomString")
 module.exports.REPLY = "reply";
 
 module.exports.reply = (bot, operation) => {
+    const regexp = new RegExp(operation.regexPattern);
 
+    bot.hears(regexp, (ctx, next) => {
+        const username = ctx.update.message.from.username;
+        if (operation.replyToUsers?.length > 0 && !operation.replyToUsers.includes(username)){
+            return next();
+        }
+        next();
+    })
 };
