c20ef6325b242bd582f5521dde250707f000d81e
Ilya Gusev <igusev2@alfabank.ru>
2021-08-10 23:29:47 +0300

    cat operation logs

--- src/operations/randomCat.js
+++ src/operations/randomCat.js
@@ -1,3 +1,4 @@
+const { logger } = require("../logger");
 const { createApi } = require('unsplash-js');
 const { config } = require('../config');
 const fetch = require('node-fetch');
@@ -6,15 +7,27 @@ module.exports.RANDOM_CAT = "randomCat";
 
 module.exports.randomCat = (bot) => {
     bot.command('cat', async (ctx) => {
+        const logMeta = {
+            botName: ctx.botInfo.username,
+            operation: module.exports.RANDOM_CAT,
+            chatName: ctx.update.message.chat.username,
+            fromUserName: ctx.update.message.from.username,
+            fromFirstName: ctx.update.message.from.first_name,
+            updateId: ctx.update.update_id
+        };
+        logger.info(`cat command detected`, logMeta)
+
         const unsplash = createApi({
             accessKey: config.unsplashAccessKey,
             fetch: fetch,
         });
 
         const catPhotoResult = await unsplash.photos.getRandom({ query: 'cat', count: 1 });
+
         const catPhotoLink = catPhotoResult?.response[0]?.urls?.regular;
         if (catPhotoLink) {
             await ctx.replyWithPhoto({ url: catPhotoLink }, {reply_to_message_id : ctx.message.message_id});
+            logger.info(`cat sent`, logMeta)
         }
     })
 }
