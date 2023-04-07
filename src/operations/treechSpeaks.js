b3873e5d675ea59b47115aa175967af951fe8ab8
Илья Гусев <i.gusev@bidzaar.com>
2023-04-07 23:08:11 +0300

    sendTreechText

--- src/operations/treechSpeaks.js
+++ src/operations/treechSpeaks.js
@@ -6,25 +6,25 @@ module.exports.TREECH_TEXT = operationName;
 module.exports.sendTreechText = (bot, operation) => {
     bot.on('inline_query', async (ctx) => {
         const responce = [];
-        const treechGeneralResp = await fetch(' http://markov/treech-general');
-        const treechGeneral = await treechGeneralResp.json();
-        treechGeneral.title = "Высер тричера"
-        responce.push(treechGeneral)
+        const treechGeneral = fetch('http://markov/treech-general')
+            .then(result => result.text())
+            .then(result => responce.push({ title : "Высер тричера", text: result}));
 
-        const treechItanResp = await fetch(' http://markov/treech-itan');
-        const treechItan = await treechItanResp.json();
-        treechItan.title = "Высер итана"
-        responce.push(treechItan)
+        const treechItanText = fetch('http://markov/treech-itan')
+            .then(result => result.text())
+            .then(result => responce.push({ title : "Высер тричера", text: result}));
+
+        await Promise.all([treechGeneral, treechItanText]);
 
         const replies = responce.map((x, index) =>{
             return {
                 type: 'article',
                 id: index,
                 title: x.title,
-                message_text: x.message
+                message_text: x.text
             }
         });
 
-        ctx.answerInlineQuery(replies, {cache_time: 5});
+        ctx.answerInlineQuery(replies, {cache_time: 0});
     })
 }
