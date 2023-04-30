ba46398c856c8878d95198e7ec24383c96408d72
Ilya Gusev <igusev2@alfabank.ru>
2023-04-30 17:43:30 +0300

    agro

--- src/operations/treechSpeaks.js
+++ src/operations/treechSpeaks.js
@@ -1,30 +1,46 @@
 const { markdownv2: format } = require('telegram-format');
-const { getAgro } = require('../util/agroStack');
+const { RefillableStack } = require('../util/agroStack');
 const fetch = require('node-fetch');
 const operationName = "treech-text"
 module.exports.TREECH_TEXT = operationName;
+let agroStack;
+let itanStack;
+let treechStack;
+
+module.exports.initStack = () => {
+    const agroFillFunc = () => fetch('http://markov/agro')
+        .then(result => result.text());
+    agroStack = new RefillableStack(agroFillFunc, 50, "agro");
+    agroStack.fill();
+
+    const itanFillFunc = () => fetch('http://markov/treech-itan')
+        .then(result => result.text());
+    itanStack = new RefillableStack(itanFillFunc, 10, "itan");
+    itanStack.fill();
+
+    const treechFillFunc = () => fetch('http://markov/treech-general')
+        .then(result => result.text());
+    treechStack = new RefillableStack(treechFillFunc, 10, "treech");
+    treechStack.fill();
+};
 
 module.exports.sendTreechText = (bot, operation) => {
     bot.on('inline_query', async (ctx) => {
         const responce = [];
-        const treechGeneral = fetch('http://127.0.0.1:5000/treech-general')
-            .then(result => result.text())
-            .then(result => responce.push({ title : "Высер тричера", text: result, img: "https://i.imgur.com/9Cm0EQv.jpg"}));
+        const itanText = itanStack.pop()
+            .then(result => responce.push({id:1, title : "Высер Итана", text: result, img: "https://i.imgur.com/CO35jw4.png"}));
+        const treechText = treechStack.pop()
+            .then(result => responce.push({id:2, title : "Высер тричера", text: result, img: "https://i.imgur.com/9Cm0EQv.jpg"}));
+        const agroText = agroStack.pop()
+            .then(result => responce.push({id:3, title : "Высер злобного девственника", text: result, img: "https://memepedia.ru/wp-content/uploads/2016/08/9df18f050741a1da79d70751018f8811.jpg"}));
 
-        const treechItanText = fetch('http://127.0.0.1:5000/treech-itan')
-            .then(result => result.text())
-            .then(result => responce.push({ title : "Высер Итана", text: result, img: "https://i.imgur.com/CO35jw4.png"}));
-
-        const agro = getAgro()
-            .then(result => responce.push({ title : "Высер девственника", text: result, img: "https://i.imgur.com/9Cm0EQv.jpg"}))
-
-        await Promise.all([treechGeneral, treechItanText, agro]);
+        await Promise.all([itanText, treechText, agroText]);
 
         const replies = responce
-            .map((x, index) => {
+            .map((x) => {
             return {
                 type: 'article',
-                id: index,
+                id: id,
                 title: x.title,
                 message_text: x.text,
                 thumb_url: x.img
--- src/util/agroStack.js
+++ src/util/agroStack.js
@@ -1,23 +1,41 @@
 const fastq = require('fastq');
-const fetch = require('node-fetch');
-const agroStack = [];
-const maxAgros = 1;
+const wt = require("worker-@thread");
 
-const fillStack = async () => {
-    do {
-        await fetch('http://127.0.0.1:5000/agro')
-            .then(result => {
-                agroStack.push(result.text())
-                console.log(`Pushed. Total agros ${agroStack.length}`);
-            });
-    } while (agroStack.length <= maxAgros)
+class RefillableStack {
+    #fillFunc;
+    #queue;
+    #stack = [];
+    #stackCapacity = 10;
+    #queueName;
+
+    constructor(fillFunc, stackCapacity, name) {
+        this.#fillFunc = fillFunc;
+        this.#queue = fastq.promise(this.fill, 1);
+        this.#stackCapacity = stackCapacity;
+        this.#queueName = name;
+    }
+
+    async pop() {
+        let elem = this.#stack.pop();
+        if (!elem){
+            console.log(`${this.#queueName} Empty. Total agros ${this.#stack.length}`);
+            elem = await this.#fillFunc();
+        }
+        if (this.#queue.length() === 0) {
+            this.#queue.push(this.fill());
+            console.log(`${this.#queueName} Enqueue. Total agros ${this.#stack.length}`);
+        }
+        console.log(`${this.#queueName} Poped. Total agros ${this.#stack.length}`);
+        return elem;
+    };
+
+    async fill() {
+        console.log(`${this.#queueName} Start fill. Total agros ${this.#stack.length}`);
+        while (this.#stack.length < this.#stackCapacity){
+            const result = await this.#fillFunc();
+            this.#stack.push(result);
+            console.log(`${this.#queueName} Pushed. Total agros ${this.#stack.length}`);
+        }
+    }
 }
-const queue = fastq.promise(fillStack, 1)
-
-module.exports.getAgro = () => {
-    const agro = agroStack.pop();
-    queue.push(fillStack);
-    return agro;
-}
-
-module.exports.fillAgro = () => queue.push(fillStack);
+module.exports.RefillableStack = RefillableStack
